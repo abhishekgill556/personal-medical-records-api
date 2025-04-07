7d10fbd1bf171f00b593bbfd8ed482deef8affca
abhishek gill <abhishekgill@rrc.ca>
2025-04-07 00:14:14 -0500

    feat: add Joi validation for appointments

--- src/api/v1/routes/appointment.routes.ts
+++ src/api/v1/routes/appointment.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import * as controller from '../controllers/appointment.controller';
+import validate from '../middleware/validate';
+import { appointmentSchema } from '../validators/appointment.validator';
 
 const router = Router();
 
+
 router.get('/', controller.getAllAppointments);
 router.get('/:id', controller.getAppointmentById);
-router.post('/', controller.createAppointment);
-router.put('/:id', controller.updateAppointment);
+router.post('/', validate(appointmentSchema), controller.createAppointment);
+router.put('/:id', validate(appointmentSchema), controller.updateAppointment);
 router.delete('/:id', controller.deleteAppointment);
 
 export default router;
